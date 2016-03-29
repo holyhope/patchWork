ef92be837702aa3d737cf8c0286c96b4aff26d1e
Maxime Pichou <maximepichou@users.noreply.github.com>
2016-03-29 12:19:53 +0200

    Add some tests to main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include "src/patchwork/Circle.h"
+#include "src/patchwork/Rectangle.h"
 
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+    Circle circle(Point(0,0), 3.);
+    cout << circle << endl;
+    cout << "Area: " << circle.area() << " Perimeter: " << circle.perimeter() << endl;
+    Circle* circleMinus = (Circle*)circle.scale(0.5);
+    cout << *circleMinus << endl;
+    cout << "AreaMinus: " << circleMinus->area() << endl;
+
+    Rectangle rectangle(Point(-2,-3), Point(2,4));
+    cout << rectangle << endl;
+    cout << "Area: " << rectangle.area() << " Perimeter: " << rectangle.perimeter() << endl;
     return 0;
 }
